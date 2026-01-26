cacb613f78937510d3209b5ad9a5cb9756932f8b
Abir Chakravorty <abirchakravorty99@gmai.com>
2026-01-26 21:13:51 +0530

    day3

--- task23/task23.js
+++ task23/task23.js
@@ -15,10 +15,7 @@ function openFeatures() {
     })
 }
 openFeatures()
-let form = document.querySelector('.addTask form')
-let taskInput = document.querySelector('.addTask form #task-input')
-let taskDetailsInput = document.querySelector('.addTask form textarea')
-let taskCheckbox = document.querySelector('.addTask form #check')
+
 
 // let currentTask = [
 //     {
@@ -73,40 +70,94 @@ let taskCheckbox = document.querySelector('.addTask form #check')
 //     },
 // ]
 
-var currentTask = []
+function todoList() {
+    let form = document.querySelector('.addTask form')
+    let taskInput = document.querySelector('.addTask form #task-input')
+    let taskDetailsInput = document.querySelector('.addTask form textarea')
+    let taskCheckbox = document.querySelector('.addTask form #check')
 
-if(localStorage.getItem('currentTask')){
-    currentTask = JSON.parse(localStorage.getItem('currentTask'));  
-}else{
-    console.log('Task List is Empty');
-}
+    var currentTask = []
 
-function renderTask() {
-    var allTask = document.querySelector('.allTask')
-    var sum = ''
-    currentTask.forEach(function (elem) {
-        sum += `<div class="task">
-                        <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
-                        <button>Mark as Completed</button>
-                    </div>`
-    })
-    allTask.innerHTML = sum
-}
-renderTask()
+    if (localStorage.getItem('currentTask')) {
+        currentTask = JSON.parse(localStorage.getItem('currentTask'));
+    } else {
+        console.log('Task List is Empty');
+    }
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault()
-    // console.log(taskInput.value);
-    // console.log(taskDetailsInput.value);
-    // console.log(taskCheckbox.checked);      
-    currentTask.push({task:taskInput.value,details:taskDetailsInput.value,imp:taskCheckbox.checked})
-    localStorage.setItem('currentTask',JSON.stringify(currentTask))
-    taskInput.value = ''
-    taskDetailsInput.value = ''
-    taskCheckbox.checked = ''
-    
+    function renderTask() {
+        var allTask = document.querySelector('.allTask')
+        var sum = ''
+        currentTask.forEach(function (elem, idx) {
+            sum += `<div class="task">
+        <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+        <button id=${idx}>Mark as Completed</button>
+        </div>`
+        })
+        allTask.innerHTML = sum
+        localStorage.setItem('currentTask', JSON.stringify(currentTask))
+
+        document.querySelectorAll('.task button').forEach(function (btn) {
+            btn.addEventListener('click', function () {
+                currentTask.splice(btn.id, 1);
+                renderTask()
+            })
+        })
+
+    }
     renderTask()
+    form.addEventListener('submit', function (e) {
+        e.preventDefault()
+        // console.log(taskInput.value);
+        // console.log(taskDetailsInput.value);
+        // console.log(taskCheckbox.checked);      
+        currentTask.push({ task: taskInput.value, details: taskDetailsInput.value, imp: taskCheckbox.checked })
+        renderTask()
+
+        taskCheckbox.checked = false
+        taskInput.value = ''
+        taskDetailsInput.value = ''
+    })
+}
+todoList()
+
+
+function dailyPlanner(){
+    var dayPlanner = document.querySelector('.day-planner')
+
+var dayPlanData = JSON.parse(localStorage.getItem('dayPlanData')) || {}
+
+var hours = Array.from({ length: 18 }, (_, idx) => `${6 + idx}:00 - ${7 + idx}:00`)
+var wholeDaySum = ''
+hours.forEach(function (elem, idx) {
+
+    var savedData = dayPlanData[idx] || ''
+
+    wholeDaySum = wholeDaySum + `<div class="day-planner-time">
+                    <p>${elem}</p>
+                    <input id=${idx} type="text" placeholder="..." value=${savedData}>
+                </div>`
 })
 
 
+dayPlanner.innerHTML = wholeDaySum
 
+
+var dayPlannerInput = document.querySelectorAll('.day-planner input')
+
+dayPlannerInput.forEach(function (elem) {
+    elem.addEventListener('input', function () {
+        dayPlanData[elem.id] = elem.value
+
+        localStorage.setItem('dayPlanData', JSON.stringify(dayPlanData))
+
+    })
+})
+}
+dailyPlanner()
+
+async function fetchQuote(){
+    let response = await fetch('https://api.quotable.io/quotes/random')
+    console.log(response);
+    
+}
+fetchQuote()
