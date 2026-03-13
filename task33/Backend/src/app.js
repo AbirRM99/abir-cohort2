86bc1d308d1228c659fd2b9a66faab814aceee7a
AbirRM99 <abirchakravorty99@gmail.com>
2026-03-13 20:56:08 +0530

    day20

--- task33/Backend/src/app.js
+++ task33/Backend/src/app.js
@@ -1,6 +1,53 @@
 const express = require("express")
 
+const noteModel = require("./models/note.model")
+const cors = require("cors")
+
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.post("/api/notes", async (req, res) => {
+    const { title, description } = req.body
+
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        message: "note ban gayayaay",
+        note
+    })
+})
+
+app.get("/api/notes", async (req, res) => {
+
+    const notes = await noteModel.find()
+    res.status(200).json({
+        message: "note mil gayahuhuu",
+        notes
+    })
+})
+
+app.delete("/api/notes/:id", async (req, res) => {
+    const id = req.params.id
+
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message: "note delete hogais"
+    })
+})
+app.patch("/api/notes/:id", async (req, res) => {
+    const id = req.params.id
+    const { description } = req.body
+
+    await noteModel.findByIdAndUpdate(id, { description })
+
+    res.status(200).json({
+        message: "note update hogaya lol"
+    })
+})
 
 
 module.exports = app;
---
+++ task33/Frontend/src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function App() {
+
+  const [notes, setNotes] = useState([
+  ])
+  console.log("helloo");
+
+  function fetchnotes() {
+    axios.get('http://localhost:3000/api/notes')
+      .then((res) => {
+        setNotes(res.data.notes)
+      })
+  }
+  useEffect(() => {
+    fetchnotes()
+  }, [])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const { title, description } = e.target.elements
+    console.log(title.value, description.value);
+
+  }
+
+
+
+  return (
+    <>
+
+      <form className="note-create-form" onSubmit={handleSubmit}>
+        <input name='title' type="text" placeholder='Enter Title' />
+        <input name='description' type="text" placeholder='Enter Description' />
+        <button>Create Note</button>
+      </form>
+      <div className="notes">
+        {
+          notes.map(note => {
+            return <div className="note">
+              <h1>{note.title}</h1>
+              <p>{note.description}</p>
+            </div>
+          })
+        }
+      </div>
+
+    </>
+  )
+}
+
+export default App
