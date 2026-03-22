84928923d17379894f65e4590fbd9ed653fef7ce
AbirRM99 <abirchakravorty99@gmail.com>
2026-03-22 23:39:08 +0530

    day23

--- task34/src/routes/auth.routes.js
+++ task34/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
+const crypto = require("crypto");
 
 const authRouter = express.Router()
 
@@ -15,19 +16,21 @@ authRouter.post("/register", async (req, res) => {
         })
     }
 
+    const hash = crypto.createHash("md5").update(password).digest("hex")
+
     const user = await userModel.create({
-        email, name, password
+        email, name, password: hash
     })
 
     const token = jwt.sign(
         {
             id: user._id,
-            email:user.email
+            email: user.email
         },
-        process.env.JWT_SECRET
+        process.env.JWT_SECRET, {expiresIn:"1h"}
     )
 
-    res.cookie("jwt_token",token)
+    res.cookie("jwt_token", token)
 
     res.status(201).json({
         message: "User Registered",
@@ -35,6 +38,57 @@ authRouter.post("/register", async (req, res) => {
         token
     })
 
+})
+
+authRouter.post("/protected", (req, res) => {
+    console.log(req.cookies);
+
+    res.status(200).json({
+        message: "this is protected route"
+    })
+})
+
+authRouter.post("/login", async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({ email })
+
+    if (!user) {
+        return res.status(404).json({
+            message: "user not found with this email address"
+        })
+    }
+    const isPasswordMatched = user.password === crypto.createHash("md5").update(password).digest("hex")
+
+    if (!isPasswordMatched) {
+        return res.status(401).json({
+            message: "Invalid Password"
+        })
+    }
+    const token = jwt.sign({
+        id: user._id,
+    }, process.env.JWT_SECRET)
+
+    res.cookie("jwt_token", token)
+
+    res.status(200).json({
+        message: "user logged in",
+        user,
+    })
+})
+
+authRouter.get("/get-me", async(req,res)=>{
+    const token = req.cookies.jwt_token
+
+   const decode = jwt.verify(token,process.env.JWT_SECRET)
+
+   const user = await userModel.findById(decode.id)
+
+   res.json({
+    name:user.name,
+    email:user.email
+   })
+   
 })
 
 module.exports = authRouter
